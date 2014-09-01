818b8b28c19ba973ccdcdbf697c393e9da90426b
cubika <receiveresume@163.com>
2014-09-01 21:24:25 +0800

    autocomplete search

--- js/call.js
+++ js/call.js
@@ -1,7 +1,10 @@
 (function() {
 	var video = $("#video"),
 		current = 0, limit = 20,
-		currentType,
+		currentType = 'src',
+		sourceList = [],
+		typeMap = {},
+		idMap = {},
 		list = $("#videoList"),
 		container = $("#result-container");
 
@@ -9,37 +12,59 @@
 		aspectRatio: 89/50
 	}).draggable();
 
+	var jxhr = [];
+	$.each(['src', 'neidi', 'gangju', 'meiju', 'hanju'], function(index, type){
+		jxhr.push($.getJSON('data/' + type + '.json', function(result) {
+			typeMap[type] = result;
+			$.extend(idMap, result);
+			$.merge(sourceList, Object.keys(result));
+		}));
+	});
+
+	$.when.apply($, jxhr).done(function() {
+		$("#search").autocomplete({
+			source: sourceList,
+			select: function(event, ui) {
+				var term = ui.item.value,
+					plid = idMap[term];
+				container.empty();
+				list.empty();
+				container.append($("<a href class='u-blka' data-plid=" + plid + ">" + term + "</a>"));
+				getVid(plid);
+			}
+		});
+		appendTV();
+	});
+
 	function appendTV() {
 		container.empty();
 		list.empty();
-		$.get('data/'+ currentType +'.json', function(data) {
-			var keys = Object.keys(data);
-			for(i=current; i<keys.length && i<current+limit; i++) {
-				container.append($("<a href class='u-blka' data-plid=" + data[keys[i]] + ">" + keys[i] + "</a>"));
-			}
-			current += limit;
+		var data = typeMap[currentType],
+			keys = Object.keys(data);
+		for(i=current; i<keys.length && i<current+limit; i++) {
+			container.append($("<a href class='u-blka' data-plid=" + data[keys[i]] + ">" + keys[i] + "</a>"));
+		}
+		current += limit;
+	}
+
+	function getVid(plid) {
+		// 获取vid
+		$.getJSON('http://pl.hd.sohu.com/videolist?playlistid=' + plid + '&pagesize=999&callback=?', function(ret) {
+			var videos = ret.videos,
+				ul = $('<ul>').addClass('f-cb');
+			$.each(videos, function(index, video) {
+				ul.append("<li><a href class='u-blka' data-vid='" + video.vid + "'>" + video.showName + "</a></li>"); 
+			});
+			ul.appendTo(list.empty()).hide().fadeIn('slow');
 		});
 	}
-	$.get('data/src.json', function(data) {
-		for(var key in data) {
-			container.append($("<a href class='u-blka' data-plid=" + data[key] + ">" + key + "</a>"));
-		}
-	});
 
 	$('#content').on('click', 'a', function() {
 		var self = this, 
 			plid = $(this).data('plid'),
 			vid = $(this).data('vid');
 		if(plid) {
-			// 获取vid
-			$.getJSON('http://pl.hd.sohu.com/videolist?playlistid=' + plid + '&pagesize=999&callback=?', function(ret) {
-				var videos = ret.videos,
-					ul = $('<ul>').addClass('f-cb');
-				$.each(videos, function(index, video) {
-					ul.append("<li><a href class='u-blka' data-vid='" + video.vid + "'>" + video.showName + "</a></li>"); 
-				});
-				ul.appendTo(list.empty()).hide().fadeIn('slow');
-			});
+			getVid(plid);
 		}else if(vid) {
 			// 获取src
 			$.getJSON('http://api.tv.sohu.com/video/playinfo/' + vid + '.json?api_key=f351515304020cad28c92f70f002261c&callback=?', function(ret) {
